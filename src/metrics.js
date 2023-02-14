e3abce12c5ae727e2b62dd5c62e0998ee352f067
Nicolas Kirchhoffer <git@nkirchho.dev>
2023-02-14 19:15:59 +0100

    :experiment: nouvel essai avec mongoose

--- src/metrics.js
+++ src/metrics.js
@@ -1,22 +1,24 @@
 import 'dotenv/config';
-import monk from 'monk';
-import { v4 as uuid } from 'uuid';
-const db = monk(process.env.MONGODB_LOGIN);
-
+import mongoose from 'mongoose';
 
 export async function storeMailInfo(doc) {
-    const mails = db.get('mails');
-    const id = monk.id();
-    
+
     try {
-        console.log(mails);
-        await mails.insert({
-            _id: id,
-            ...doc
+        await mongoose.connect(process.env.MONGODB_URL);
+
+        const mailSchema = new mongoose.Schema({
+            inboundSize: Number,
+            outboundSize: Number,
+            recipientsCount: Number,
+            sender: String,
+            hasAttachments: Boolean
         });
+
+        const Mail = mongoose.model('Mail', mailSchema);
+
+        const mail = new Mail(doc);
+        await mail.save();
     } catch (e) {
         console.error(e);
     }
-
-    db.close();
 }
