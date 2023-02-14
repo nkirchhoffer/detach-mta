79b5dff53c3bbd4609d12e3522efa528fb5ebc3b
Nicolas Kirchhoffer <git@nkirchho.dev>
2023-02-14 18:53:44 +0100

    :experiment: test de ObjectID manuel

--- src/metrics.js
+++ src/metrics.js
@@ -1,9 +1,15 @@
 import 'dotenv/config';
 import monk from 'monk';
+import { v4 as uuid } from 'uuid';
 const db = monk(process.env.MONGODB_LOGIN);
 
-const mails = db.get('mails');
 
-export function storeMailInfo(document) {
-    mails.insert(document);
+export function storeMailInfo(doc) {
+    const mails = db.get('mails');
+    const id = monk.id(uuid());
+    console.log(mails);
+    mails.insert({
+        _id: id,
+        ...document
+    });
 }
